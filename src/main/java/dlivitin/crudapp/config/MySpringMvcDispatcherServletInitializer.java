8d18ca2b48712169cac88341a15f8017af79c033
Daniil <danya_livitin@mail.ru>
2021-07-23 21:56:49 +0300

    First commit

---
+++ src/main/java/dlivitin/crudapp/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,23 @@
+// We use this class instead of using web.xml
+// So this class is used to configure web server
+
+package dlivitin.crudapp.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
